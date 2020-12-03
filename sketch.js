678deadb82495d8684f7a6d74f57a8c0e452fa91
kanishkarepo <kanishkamukesh007@gmail.com>
2020-12-03 14:07:26 -0500

    added logo for app

--- sketch.js
+++ sketch.js
@@ -1,9 +1,46 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+var logo, logoImage ;
+var gameState = "menuScreen";
+var menuIcon, menuImage;
+
+function preload()
+{
+ logoImage = loadImage("images/LOGO.jpeg")
+ menuImage = loadImage("images/menu bar icon.png")
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function setup() {
+	createCanvas(650, 650);
+
+
+	//engine = Engine.create();
+	//world = engine.world;
+
+	//Create the Bodies Here.
+	logo = createSprite(250, 300, 700, 600);
+	logo.addImage("logo" ,logoImage);
+	logo.scale = 0.5;
+
+	menuIcon = createSprite(620, 45,10, 10);
+	menuIcon.addImage("icon", menuImage);
+	menuImage.scale = 0.1;
+
+  
 }
+
+
+function draw() {
+  
+  background(255, 255, 255);
+  
+  if(keyDown("space")){
+    logo.visible = false;
+	gameState = "menuScreen";
+  }
+
+  drawSprites();
+ 
+}
+
+
+
